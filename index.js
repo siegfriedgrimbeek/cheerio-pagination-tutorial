60a77835408501d25246d65f02304653b53db4f9
Siegfried <siegfried.grimbeek@gmail.com>
2018-05-12 18:00:47 +0200

    code cleaned

--- index.js
+++ index.js
@@ -4,29 +4,25 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const chalk = require('chalk')
 
-const parsedResults = []
-const resultsLimit = 10
 const url = 'http://listverse.com/'
 const outputFile = 'data.json'
+const parsedResults = []
+const pageLimit = 10
 let pageCounter = 0
 let resultCount = 0
 
-console.log(chalk.white.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
+console.log(chalk.yellow.bgBlue(`\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`))
 
 const getWebsiteContent = async (url) => {
   try {
     const response = await axios.get(url)
     const $ = cheerio.load(response.data)
 
-    // Pagination Elements Link
-    const nextPageLink = $('.pagination').find('.curr').parent().next().find('a').attr('href')
-
     // New Lists
     $('.wrapper .main .new article').map((i, el) => {
       const count = resultCount++
       const title = $(el).find('a').attr('href')
       const url = $(el).find('h3').text()
-
       const metadata = {
         count: count,
         title: title,
@@ -35,13 +31,16 @@ const getWebsiteContent = async (url) => {
       parsedResults.push(metadata)
     })
 
+    // Pagination Elements Link
+    const nextPageLink = $('.pagination').find('.curr').parent().next().find('a').attr('href')
+    console.log(chalk.cyan(`  Scraping: ${nextPageLink}`))
     pageCounter++
 
-    console.log(chalk.cyan(`  Scraping: ${nextPageLink}`))
-    if (pageCounter === resultsLimit) {
+    if (pageCounter === pageLimit) {
       exportResults(parsedResults)
       return false
     }
+
     getWebsiteContent(nextPageLink)
   } catch (error) {
     exportResults(parsedResults)
@@ -53,9 +52,8 @@ const exportResults = (parsedResults) => {
   fs.writeFile(outputFile, JSON.stringify(parsedResults, null, 4), (err) => {
     if (err) {
       console.log(err)
-      return false
     }
-    console.log(chalk.white.bgBlue(`\n ${chalk.underline.bold(parsedResults.length)} Results exported successfully to ${chalk.underline.bold(outputFile)}\n`))
+    console.log(chalk.yellow.bgBlue(`\n ${chalk.underline.bold(parsedResults.length)} Results exported successfully to ${chalk.underline.bold(outputFile)}\n`))
   })
 }
 
